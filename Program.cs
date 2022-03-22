0dcacf48a5bb33195e6ec6dceac9c8aca850fce1
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-03-22 12:46:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedivanje3
+{
+    class Brojevi
+    {
+        double broj;
+        public double Broj { get => broj; set => broj = value; }
+        public virtual double ApsVrijednost()
+        {
+            return 0;
+        }
+    }
+    class Cijeli : Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            return Broj < 0 ? -Broj : Broj; // Skraceni if
+        }
+    }
+    class Pozitivni : Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            return Broj;
+        }
+    }
+    class Negativni : Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            return Broj * -1;
+        }
+    }
+
+    class Decimalni : Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            return Broj < 0 ? -Broj : Broj;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozitivni x = new Pozitivni();
+            x.Broj = 100;
+            Console.WriteLine(x.ApsVrijednost());
+            Negativni y = new Negativni();
+            y.Broj = -100;
+            Console.WriteLine(y.ApsVrijednost());
+            Decimalni z = new Decimalni();
+            z.Broj = 100.45;
+            Console.WriteLine(z.ApsVrijednost());
+
+
+            Console.ReadKey();
+        }
+    }
+}
